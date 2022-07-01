f56bf1e8566bdefa8c3a7fc0b15081f373a68f45
rada wimolkhajornkitti <fainrada@gmail.com>
2022-07-01 13:19:30 +0700

    Post data & display error from API response

--- register/script/index.js
+++ register/script/index.js
@@ -2,13 +2,36 @@
 var submit_button = document.getElementById("submit-button");
 var field_id = ["fname","lname","uname","email","pw","confirmpw"];
 
+async function postData(data) {
+    console.log("start posting data")
+    await fetch(
+      'http://isd-test.cucheck.in/users',
+        {
+            method: "POST",
+            headers: {'accept': '*/*','Content-Type': 'application/json'}, 
+            body: JSON.stringify(data)
+        }
+    ).then(res => {
+        console.log("Request complete - response: ", res);
+        if(!res.ok) {
+            return res.text().then(text => { throw new Error(text) })
+        }
+        else {
+            window.location.href = "../name-list/index.html";  // change page
+        }    
+    })
+    .catch(err => {
+        var jsonRes = JSON.parse(err.message);
+        alert(jsonRes.message[0]);
+    });
+}
+
 submit_button.onclick = function(){
-    
     var input_field = [];
     for(var i=0;i<field_id.length;i++){
         input_field[i] = document.getElementById(field_id[i]);
         if(input_field[i].value.length == 0){
-            alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+            alert("please fill in all fields");
             return;
         }
     }
@@ -16,17 +39,25 @@ submit_button.onclick = function(){
     var validEmailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (!input_field[2].value.match(validUsernameRegex)){
-        alert("ชื่อผู้ใช้ต้องประกอบด้วยตัวอักษรภาษาอังกฤษหรือตัวเลขเท่านั้น");
+        alert("username must contain only letters and numbers");
     }
     else if (!input_field[3].value.match(validEmailRegex)){
-        alert("อีเมลผิดรูปแบบ");
+        alert("invalid email format");
     }
-    else if (!input_field[4].value === input_field[5].value){
-        alert("รหัสผ่านกับยืนยันรหัสผ่านไม่ตรงกัน")
+    else if (!(input_field[4].value === input_field[5].value)){
+        alert("password doesn't match confirmation");
     }
     else{
         console.log("valid input");
         //TODO: call api//
-        window.location.href = "../name-list/index.html";  // change page
+        var data = { 
+            username: input_field[2].value, 
+            name: input_field[0].value, 
+            surname: input_field[1].value, 
+            email: input_field[3].value, 
+            password: input_field[4].value };
+        console.log(data);
+        console.log(JSON.stringify(data));
+        postData(data);
     } 
 }
